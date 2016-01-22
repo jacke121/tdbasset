67982e92b76ae48fd9fe13ab12c5813f429e1f65
jacke121 <jacke121@126.com>
2016-01-22 22:13:40 +0800

    修改首页

--- resources/views/member/index/forget.blade.php
+++ resources/views/member/index/forget.blade.php
@@ -3,8 +3,8 @@
 <head>
 <meta charset="utf-8">
 <meta name="_token" content="{{ csrf_token() }}"/>
-<title>个人中心</title>
-<link rel="stylesheet" type="text/css" href="{{asset('/css/personal center.css')}}">
+    <title>找回密码</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('/')}}css/retrieve password.css">
     <style>
         span.error {
             padding-left: 16px;
@@ -20,10 +20,19 @@
 <script src="{{ asset('/js/jquery.validate.min.js')}}" type="text/javascript"></script>
 <script type="text/javascript">
     var currentnum=1;
-    function modifypwd() {
-        $(".phonecon").hide();
-        $(".pwdcon").show();
-
+    function findPwdStep(step) {
+        if(step==1){
+            $(".step1").hide();
+            $(".step2").show();
+        }
+        else if(step==2){
+            $(".step2").hide();
+            $(".step3").show();
+        }
+        else if(step==3){
+            $(".step3").hide();
+            $(".step4").show();
+        }
     }
 
     function modifymobile(step) {
@@ -58,8 +67,6 @@
         }
     }
     $(function () {
-         $(".phonecon").show();
-        setindex("cen_security");
             $("#formpwd").validate({
                 errorClass: "error",
                 errorElement: "span",
@@ -195,132 +202,74 @@
     }
 </script>
 </head>
-
 <body>
     @include('themes.default.top')
 <!--main-->
-    <div class="maincon">
-        <!-- @include('member.left_nav') -->
-<!--安全中心-->
-        <div class="mainr">
-    <div class="main_anquan">
-        <h3 class="main_anquan">忘记密码</h3>
-       	<div class="phonecon">
-       	<!-- 第一步 -->
-       	<div class="phonecon1">
-			 <table cellspacing="0" class="tabletit1">
-			    <tbody><tr>
-			      <td class="tdspe">验证原手机号码</td>
-			      <td>验证新手机号码</td>
-			      <td>成功</td>
-			    </tr>
-			  </tbody></table>
-			  <img src="{{ asset('/')}}images/anquan1.png">
-            {!! Form::open(['id'=>'step2','url' => '/auth/modifymobile', 'method' => 'post','class'=>'form-horizontal']) !!}
-            <input type="hidden" name="step" value="2">
-			  <div class="int">
-                    <span class="intspan">手机号码</span>
-                    <input id="mobile" name="mobile" type="text" value="" readonly>
-                    <span id="btnSendCode1" onclick="sendMessage(1)" class="code">获取验证码</span>
-                    <span class="code1">59s后重新发送</span>
-                    <span class="ts"></span>
-              </div>
-              <div class="int">
-                    <span class="intspan">短信验证码</span>
-                    <input name="checkcode" type="text">
-                    <span class="ts"></span>
-              </div>
-              <p class="phonestep1">
-                <a href="javascript:;" onclick="modifymobile(2)" class="phone1btn">下一步</a>
-              </p>
-            {!! Form::close() !!}
-        </div>
-        <!-- 第二步 -->
-			  <div class="bgbg">
-			  <span class="bgts">温馨提示</span>
-<p>1.请填写真实有效的手机号，手机号将作为验证用户身份发重要手段。</p>
-<p>2.债工厂会对用户的所有资料进行严格保密。</p>
-<p>3.手机认证过程遇到问题，请联系客服 000-111-222</p>
-			  </div>
-       	</div>
-       	<p class="an_list list_spe"><a href="javascript:;" onclick="modifypwd()" class="lista pwdx">修改</a></p>
-       	<div class="pwdcon">
-            {!! Form::open(['id'=>'formpwd','url' => '/auth/modifypwd', 'method' => 'post','class'=>'form-horizontal']) !!}
-       	<!-- 第一步 -->
-       	<div class="pwdcon1">
-			  <div class="int">
-                    <span class="intspan">原登录密码</span>
-                    <input type="password" name="oldpassword" class="oldpassword">
-                    <span class="ts"></span>
-              </div>
-              <div class="int">
-                    <span class="intspan">新登录密码</span>
-                    <input id="password" type="password" name="password">
-                    <span class="ts"></span>
-              </div>
-              <div class="int">
-                    <span class="intspan">再次输入新登录密码</span>
-                    <input type="password" name="confirm_password">
-                    <span class="ts"></span>
-              </div>
-              <p class="phonestep1">
-                  <button type="submit" class="pwd1btn">修改登录密码</button>
-              </p>
-        </div>
-            {!! Form::close() !!}
-        <!-- 第二步 -->
-       	<div class="pwdcon2">
-			  恭喜您成功修改登录密码！！！
-        </div>
-      			<div class="bgbg">
-			  <span class="bgts">温馨提示</span>
-<p>1请牢记您设置的新密码，登录密码可通过密码找回功能找回。</p>
-<p>2.邮箱认证过程遇到问题，请联系客服 000-111-222</p>
-			  </div>
-       	</div>
-       	<p class="an_list" style="display: none;"><span class="listtit">邮箱认证：</span>已设置<a href="javascript:void(0);" class="lista emailx">修改</a></p>
-       	<div class="emailcon">
-       	<!-- 第一步 -->
-       	<div class="emailcon1">
-			  <div class="int">
-                    <span class="intspan">原邮箱地址</span>
-                    <input type="text" value="" readonly>
-                    <span class="ts"></span>
-              </div>
-              <div class="int">
-                    <span class="intspan">新邮箱地址</span>
+    <!--广告位-->
+    <div class="banner">
+        <img src="../images/login_ggt.jpg">
+    </div>
+    <!--找回密码部分-->
+    <div class="repwd">
+        <div class="repwdcon">
+            <h3 class="repwdtit">找回密码</h3>
+            <div class="step1">
+                <div class="repwd_step1"></div>
+                <div class="int">
+                    <span class="intspan">用户名</span>
                     <input type="text">
-                    <span class="ts"></span>
-              </div>
-              <p class="phonestep1">
-                <a href="#" class="emailbtn">发送验证邮件</a>
-              </p>
+                    <span class="ts" style="display: none;">提示提示……</span>
+                </div>
+                <div class="int">
+                    <span class="intspan">验证码</span>
+                    <input type="text" class="intspe">
+                    <a href="javascript:;" class="code"></a>
+                    <span class="ts" style="display: none;">提示提示……</span>
+                </div>
+                <p class="reg1next">
+                    <a href="javascript:;" onclick="findPwdStep(1)" class="step1btn">下一步</a>
+                </p>
+            </div>
+            <div class="step2">
+                <div class="repwd_step2"></div>
+                <p class="step2p">短信验证码已发送，请注意查收</p>
+                <div class="int">
+                    <span class="intspan">验证码</span>
+                    <input type="text">
+                    <span class="code1"><span>59s</span></span>
+                    <span class="ts" style="display: none;">提示提示……</span>
+                </div>
+                <p class="step2pts">如果没有收到短信，您可在<span>60s</span>后获取语音验证码</p>
+                <p class="reg1next">
+                    <a href="javascript:;" onclick="findPwdStep(2)" class="step2btn">下一步</a>
+                </p><p class="zhao">如您无法使用上述方法找回，请联系客服400-058-9555或重新注册</p>
+            </div>
+            <div class="step3">
+                <div class="repwd_step3"></div>
+                <div class="int">
+                    <span class="intspan">新登录密码</span>
+                    <input type="password">
+                    <span class="ts" style="display: none;">提示提示……</span>
+                </div>
+                <div class="int">
+                    <span class="intspan">确认新登录密码</span>
+                    <input type="password">
+                    <span class="ts" style="display: none;">提示提示……</span>
+                </div>
+                <p class="reg1next">
+                    <a href="javascript:;" onclick="findPwdStep(3)" class="step3btn">确认</a>
+                </p>
+            </div>
+            <div class="step4">
+                <div class="repwd_step4"></div>
+                <div class="step4con">
+                    <p class="step4con1">设置成功，请牢记新的登录密码</p>
+                    <p class="step4con2"><span id="jumpTo">5</span>自动跳转至<a href="http://www.qt361.com/login/toLogin">登录页</a></p>
+                </div>
+            </div>
         </div>
-      			<div class="bgbg">
-			  <span class="bgts">温馨提示</span>
-<p>1.请填写真实有效的邮箱地址，您可以通过邮箱进行找回密码等功能。</p>
-<p>2.青苔债管家会对用户的所有资料进行严格保密。</p>
-<p>3.邮箱认证过程遇到问题，请联系客服 000-111-222</p>
-			  </div>
-       	</div>
-     <!-- 弹出层-->
-    <div class="tan" style="display:none;">
-        <p class="tants">激活成功！</p>
-        <p class="tants1">
-        已成功发送验证邮件到您的新邮箱，<br>
-    请在24小时内进入新邮箱激活。
-         </p><p class="tanp">
-         <input type="submit" class="tanbtn" value="确认">
-         </p>
-    </div>
-       	<p class="an_list"><span class="listtit">资格认证：</span>
-       			未提交认证申请
-        </p>
-    </div>
     </div>
         <div style="clear:both;"></div>
-</div>
-
 <!--foot部分-->
 	@include('themes.default.foot')
 </body>
