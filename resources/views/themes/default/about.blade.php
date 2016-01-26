d7863dc9c1fc9496fc13a861aeb9b757f47a9ee4
km2015km <okk12345@gamil.com>
2016-01-26 19:40:39 +0800

    2016-01-26 19:40  最新修改

--- resources/views/themes/default/about.blade.php
+++ resources/views/themes/default/about.blade.php
@@ -1,26 +1,39 @@
 @extends('themes.default.layouts')
 @include('themes.default.top')
 @section('content')
-    <link rel="stylesheet" type="text/css" href="{{ asset('/css/list.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('/css/bootstrap.min.css') }}">
-    <section class="container content">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/about.css') }}">
 
-        <div class="columns about">
-            <div class="row">
-               <div class="col-sm-2">
-                   <ul class="list-unstyled about_nav">
-                       <li class="navTitle"><a>关于我们</a></li>
-                       @foreach($aboutList as $k=> $v)
-                       <li id="about_nav_{{$v->id}}"><a href="{{ url('about?id='.$v->id) }}">{{$v->title}}</a></li>
-                       @endforeach
-                   </ul>
-               </div>
-                <div class="col-sm-10">
-                    {!! $about->content !!}
+    <div class="content">
+        <div class="center">
+            <div class="con24">
+                <!--左边模块 start-->
+                <div class="con_l">
+                    <ul>
+                        <a href="#"><li class="cur1">{{$v->title}}</li>
+                            <i class="cura"></i>
+                        </a>
+                        @foreach($aboutList as $k=> $v)
+                            <a href="{{ url('about?id='.$v->id) }}"> <li id="about_nav_{{$v->id}}">{{$v->title}}</li><i ></i></a>
+                        @endforeach
+                    </ul>
                 </div>
+                <!--左边模块end-->
+
+                <!--右边模块 start-->
+                <div class="con_r">
+                    <h2>
+                        关于我们
+                    </h2>
+                    <div class="main">
+                        <div class="wu">
+                        </div>
+                        {!! $about->content !!}
+                    </div>
+                </div>
+                <!--右边模块 end-->
             </div>
-            </div>
-    </section>
+        </div>
+    </div>
 
     <div class="clearfix"></div>
     @include('themes.default.foot')
@@ -29,7 +42,7 @@
     $(document).ready(function(){
         var aboutPath = "{{$id}}";
         aboutPath = "#about_nav_"+aboutPath;
-        $(aboutPath).addClass("current");
+        $(aboutPath).addClass("cur1");
        });
     </script>
 @endsection
