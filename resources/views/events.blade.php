96ca9f7e9beaf2bcea556e0ada77afbc1f7b8915
jacke121 <jacke121@126.com>
2016-02-04 00:20:00 +0800

    增加广播

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -11,8 +11,25 @@
         </ul>
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.3.6/socket.io.min.js"></script>
+
         <script>
-            var socket = io('http://localhost:3000');
+            //js获取项目根路径，如： http://localhost:8083/uimcardprj
+            function getRootPath(){
+                //获取当前网址，如： http://localhost:8083/uimcardprj/share/meun.jsp
+                var curWwwPath=window.document.location.href;
+                //获取主机地址之后的目录，如： uimcardprj/share/meun.jsp
+                var pathName=window.document.location.pathname;
+                var pos=curWwwPath.indexOf(pathName);
+                //获取主机地址，如： http://localhost:8083
+                var localhostPaht=curWwwPath.substring(0,pos);
+                //获取带"/"的项目名，如：/uimcardprj
+                var projectName=pathName.substring(0,pathName.substr(1).indexOf('/')+1);
+                if(projectName!="tdbasset"){
+                    projectName="";
+                }
+                return(localhostPaht+projectName);
+            };
+            var socket = io(getRootPath()+':3000');
 
             socket.on("test-channel:App\\Events\\UserRegisteredEvent", function(message){
                 console.log(message);
